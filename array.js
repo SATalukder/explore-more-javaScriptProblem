2ceaa11d0c130b66a41e5b64ff24a7286a08042d
SATalukder <shajibahmad152@gmail.com>
2023-01-27 01:20:41 +0600

    check Array

--- array.js
+++ array.js
@@ -4,4 +4,39 @@ const isIndependent = true;
 const student = { id: 121, class: 11, name: 'Agun' };
 const friends = [13, 14, 11, 17, 21, 15, 16, 20];
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
 console.log(typeof country);
+console.log(typeof age);
+console.log(typeof add);
+console.log(typeof student);
+console.log(typeof friends);
+console.log(typeof isIndependent);
+//check array using Array.isArray
+console.log(Array.isArray(friends));
+
+
+
+//--------------------
+
+//check something
+
+console.log(friends.includes(11));
+console.log(friends.includes(19));
+
+//almost semiler
+
+if (friends.indexOf(19) !== -1) {
+    //ei babe check kora
+}
+
+//concat : 
+
+const newFriendsAge = [12, 13, 11, 13]
+
+const allFriends = newFriendsAge.concat(friends);
+
+console.log(allFriends);
+
